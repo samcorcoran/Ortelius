d8834a08095cba52a7935a4fb4b1fc98e7f1e6be
Sam Corcoran <samuel.corcoran@gmail.com>
2018-08-27 10:19:06 +0100

    Added lighting with rotating sun to world shader

---
+++ Assets/Scripts/SunMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour {
+
+    // TODO: Rotation around X axis changes season (who gets permadark winters)
+    // TODO: Rotation around Y axis is changes time of day (which side is shadowed)
+
+    public float MovementSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.RotateAround(Vector3.up, MovementSpeed*Time.deltaTime);
+    }
+}
