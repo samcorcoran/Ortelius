e193319457eb56be2625e664b9797187ecbe6931
Sam Corcoran <samuel.corcoran@gmail.com>
2018-08-27 20:00:58 +0100

    Fix deprecation warning on rotation

--- Assets/Scripts/SunMovement.cs
+++ Assets/Scripts/SunMovement.cs
@@ -16,6 +16,6 @@ public class SunMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.RotateAround(Vector3.up, MovementSpeed*Time.deltaTime);
+        transform.Rotate(Vector3.up, MovementSpeed*Time.deltaTime);
     }
 }
